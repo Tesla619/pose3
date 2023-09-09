b4d520aa1110160bac10b125246174aaf9c50e70
Karl Spiteri <karl.spiteri.e21724@mcast.edu.mt>
2023-09-09 17:25:36 +0200

    update marker generation

--- charuco_board.py
+++ charuco_board.py
@@ -1,7 +1,8 @@
+import os
 import cv2
 import cv2.aruco as aruco
 
-def generate_charuco_board(paper_width, paper_height, fixed_square_size, dpi, margin, output_filename):
+def generate_charuco_board(paper_width, paper_height, fixed_square_size, dpi, margin, output_path, output_filename):
     # Adjust the paper dimensions to account for the margin
     adjusted_paper_width = paper_width - 2 * margin
     adjusted_paper_height = paper_height - 2 * margin
@@ -21,9 +22,11 @@ def generate_charuco_board(paper_width, paper_height, fixed_square_size, dpi, ma
 
     # Generate the Charuco board image
     board_image = board.draw(image_size)
-
+    
+    os.makedirs(output_path, exist_ok=True)
+    
     # Save the board image to a file
-    cv2.imwrite(output_filename, board_image)
+    cv2.imwrite(output_path + '\\' + output_filename, board_image)
     
     # Display the board (optional)
     cv2.imshow(f'Charuco Board - {output_filename}', board_image)
@@ -34,13 +37,13 @@ def generate_charuco_board(paper_width, paper_height, fixed_square_size, dpi, ma
 dpi = 1000
 
 # A1 paper dimensions in meters, fixed square size, and margin
-generate_charuco_board(0.841, 0.594, 0.10, dpi, 0.02, 'To Print\\A1\\A1_charuco_board.png')
+generate_charuco_board(0.841, 0.594, 0.10, dpi, 0.02, 'To Print\\A1', 'A1_charuco_board.png')
 
 # For A2
-generate_charuco_board(0.420, 0.594, 0.10, dpi, 0.02, 'To Print\\A2\\A2_charuco_board.png')
+generate_charuco_board(0.420, 0.594, 0.10, dpi, 0.02, 'To Print\\A2', 'A2_charuco_board.png')
 
 # For A3
-generate_charuco_board(0.297, 0.420, 0.09, dpi, 0.02, 'To Print\\A3\\A3_charuco_board.png')
+generate_charuco_board(0.297, 0.420, 0.09, dpi, 0.02, 'To Print\\A3', 'A3_charuco_board.png')
 
-# A4 paper dimensions in meters, fixed square size, and margin
-generate_charuco_board(0.210, 0.297, 0.08, dpi, 0.01, 'To Print\\A4\\A4_charuco_board.png')
+# For A4
+generate_charuco_board(0.210, 0.297, 0.08, dpi, 0.01, 'To Print\\A4', 'A4_charuco_board.png')
--- markers-new.py
+++ markers-new.py
@@ -12,57 +12,72 @@ def generate_aruco_marker(marker_id, markerSize, marker_size_cm, resolution_ppcm
     cv2.imwrite(image_path, marker_image)
 
 def generate_white_box(box_size_cm, resolution_ppcm, output_path, border_thickness=2):
-    # Calculate the box size in pixels
     box_size_pixels = int(box_size_cm * resolution_ppcm)
-
-    # Create a white box image
     box_image = np.ones((box_size_pixels, box_size_pixels), dtype=np.uint8) * 255
-
-    # Add a black border
-    box_image[0:border_thickness, :] = 0  # Top border
-    box_image[-border_thickness:, :] = 0  # Bottom border
-    box_image[:, 0:border_thickness] = 0  # Left border
-    box_image[:, -border_thickness:] = 0  # Right border
-
-    # Save the white box image with a black border to a file
+    box_image[0:border_thickness, :] = 0
+    box_image[-border_thickness:, :] = 0
+    box_image[:, 0:border_thickness] = 0
+    box_image[:, -border_thickness:] = 0
     cv2.imwrite(output_path, box_image)
 
-    
-def generate_A4_sheet_with_markers(box_marker_paths, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, output_path, margin_cm=1):
+def generate_A4_sheet_with_markers(box_marker_paths, start_idx, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, output_path, current_page, total_pages):
     A4_width_pixels = int(A4_width_cm * resolution_ppcm)
     A4_height_pixels = int(A4_height_cm * resolution_ppcm)
     A4_image = np.ones((A4_height_pixels, A4_width_pixels), dtype=np.uint8) * 255
 
     box_marker_pixel_size = int(box_size_cm * resolution_ppcm)
     
-    # Calculate the number of markers that can fit horizontally and vertically
     num_markers_horizontal = int(A4_width_cm // (box_size_cm + margin_cm))
     num_markers_vertical = int(A4_height_cm // (box_size_cm + margin_cm))
-    
-    # Calculate total space required for markers and margins
-    total_width_required = num_markers_horizontal * (box_size_cm + margin_cm) - margin_cm  # minus margin_cm to remove the margin after the last marker
+    total_width_required = num_markers_horizontal * (box_size_cm + margin_cm) - margin_cm
     total_height_required = num_markers_vertical * (box_size_cm + margin_cm) - margin_cm
     
-    # Calculate starting coordinates to draw markers such that they are centered on the A4 sheet
     start_x = (A4_width_cm - total_width_required) / 2 * resolution_ppcm
     start_y = (A4_height_cm - total_height_required) / 2 * resolution_ppcm
 
     y_coord = start_y
+    current_idx = start_idx
     for _ in range(num_markers_vertical):
         x_coord = start_x
         for _ in range(num_markers_horizontal):
-            if not box_marker_paths:
+            if current_idx >= len(box_marker_paths):
                 break
-            box_marker_path = box_marker_paths.pop(0)
+            box_marker_path = box_marker_paths[current_idx]
             box_marker_img = cv2.imread(box_marker_path, cv2.IMREAD_GRAYSCALE)
             A4_image[int(y_coord):int(y_coord+box_marker_pixel_size), int(x_coord):int(x_coord+box_marker_pixel_size)] = box_marker_img
             x_coord += (box_size_cm + margin_cm) * resolution_ppcm
+            current_idx += 1
         y_coord += (box_size_cm + margin_cm) * resolution_ppcm
 
+
+    # Calculate the end_idx
+    end_idx = start_idx + num_markers_horizontal * num_markers_vertical - 1
+    if end_idx >= len(box_marker_paths):
+        end_idx = len(box_marker_paths) - 1
+
+    
+    # Extracting IDs from the filename
+    start_id = box_marker_paths[start_idx].split('_id')[1].split('_')[0]
+    end_id = box_marker_paths[end_idx].split('_id')[1].split('_')[0]
+
+    # Constructing the marker name for the text overlay
+    marker_name = f"{markerSizeStart}x{markerSizeStart}_id{start_id}_-_{end_id}"   
+    
+    # Adjusting the font size and thickness
+    fontScale = 0.75  # adjust this value was 0.5
+    thickness = 3    # adjust this value was 2
+    
+    # Superimposing the name of the markers
+    cv2.putText(A4_image, marker_name, (30, A4_height_pixels - 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 0, 0), thickness, cv2.LINE_AA)
+    
+    # Superimposing the page number
+    page_number = f"Page {current_page}/{total_pages}"
+    cv2.putText(A4_image, page_number, (A4_width_pixels - 200, A4_height_pixels - 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0,0,0), thickness, cv2.LINE_AA)
+    
+    # Saving the A4 sheet with markers
     cv2.imwrite(output_path, A4_image)
 
 
-# Get user input for marker_size_cm
 try:
     marker_size_cm = float(input("Enter the marker size in cm: "))
 except ValueError:
@@ -70,10 +85,9 @@ except ValueError:
     exit()
 
 box_size_cm = marker_size_cm + 0.4
-
-# Check if the markers fit on A4 paper
 margin_cm = 1
 A4_width_cm, A4_height_cm = 21 - 2, 29.7 - 2
+
 num_markers_horizontal = int(A4_width_cm // (marker_size_cm + margin_cm))
 num_markers_vertical = int(A4_height_cm // (marker_size_cm + margin_cm))
 
@@ -81,53 +95,48 @@ if num_markers_horizontal < 1 or num_markers_vertical < 1:
     print("The markers are too large for the A4 size.")
     exit()
 
-# Settings
 resolution_ppcm = 37.8
-output_path = f"Markers/{marker_size_cm}cm/white_box.png"
+base_path = os.path.join("Markers", f"{marker_size_cm}cm")
+output_path = os.path.join(base_path, "white_box.png")
 markerSizeStart, markerSizeEnd = 6, 6
 num_markers = 20
 
-os.makedirs(f"Markers/{marker_size_cm}cm/Generated_Markers", exist_ok=True)
-os.makedirs(f"Markers/{marker_size_cm}cm/Generated_Box_Markers", exist_ok=True)
+os.makedirs(os.path.join(base_path, "Generated_Markers"), exist_ok=True)
+os.makedirs(os.path.join(base_path, "Generated_Box_Markers"), exist_ok=True)
 generate_white_box(box_size_cm, resolution_ppcm, output_path)
 
 for i in range(markerSizeStart, markerSizeEnd+1):
     for j in range(num_markers):
-        marker_id = j
-        marker_path = f"Markers/{marker_size_cm}cm/Generated_Markers/{i}x{i}_marker_id{j}_{marker_size_cm}cm.png"
-        generate_aruco_marker(marker_id, i, marker_size_cm, resolution_ppcm, marker_path)
+        marker_path = os.path.join(base_path, "Generated_Markers", f"{i}x{i}_marker_id{j}_{marker_size_cm}cm.png")
+        generate_aruco_marker(j, i, marker_size_cm, resolution_ppcm, marker_path)
 
 for i in range(markerSizeStart, markerSizeEnd+1):
     for j in range(num_markers):
-        background = cv2.imread(f"Markers/{marker_size_cm}cm/white_box.png")
-        marker_path = f"Markers/{marker_size_cm}cm/Generated_Markers/{i}x{i}_marker_id{j}_{marker_size_cm}cm.png"
+        background = cv2.imread(output_path)
+        marker_path = os.path.join(base_path, "Generated_Markers", f"{i}x{i}_marker_id{j}_{marker_size_cm}cm.png")
         overlay = cv2.imread(marker_path)
-
         y, x = tuple((background.shape[d] - overlay.shape[d]) // 2 for d in range(2))
         h, w = overlay.shape[:2]
         background[y:y+h, x:x+w] = overlay
-        box_marker_path = f"Markers/{marker_size_cm}cm/Generated_Box_Markers/{i}x{i}_marker_id{j}_{marker_size_cm}cm.png"
+        box_marker_path = os.path.join(base_path, "Generated_Box_Markers", f"{i}x{i}_marker_id{j}_{marker_size_cm}cm.png")
         cv2.imwrite(box_marker_path, background)
 
-markers_per_row = int(A4_width_cm // (box_size_cm + 1))
-markers_per_col = int(A4_height_cm // (box_size_cm + 1))
-sheet_folder = f"Markers/{marker_size_cm}cm/{marker_size_cm}cm sheets"
+sheet_folder = os.path.join(base_path, f"{marker_size_cm}cm sheets")
 os.makedirs(sheet_folder, exist_ok=True)
 
-box_marker_paths = [f"Markers/{marker_size_cm}cm/Generated_Box_Markers/{i}x{i}_marker_id{j}_{marker_size_cm}cm.png" 
+box_marker_paths = [os.path.join(base_path, "Generated_Box_Markers", f"{i}x{i}_marker_id{j}_{marker_size_cm}cm.png") 
                     for i in range(markerSizeStart, markerSizeEnd+1) for j in range(num_markers)]
 
-sheet_count = len(box_marker_paths) // (markers_per_row * markers_per_col)
-for i in range(sheet_count):
-    markers_for_this_sheet = box_marker_paths[i * markers_per_row * markers_per_col : (i+1) * markers_per_row * markers_per_col]
-    output_sheet_path = f"Markers/{marker_size_cm}cm/{marker_size_cm}cm sheets/sheet_{i+1}.png"
-    generate_A4_sheet_with_markers(markers_for_this_sheet, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, output_sheet_path)
+total_pages = (len(box_marker_paths) + num_markers_horizontal * num_markers_vertical - 1) // (num_markers_horizontal * num_markers_vertical)
+sheet_count = len(box_marker_paths) // (num_markers_horizontal * num_markers_vertical)
+remainder = len(box_marker_paths) % (num_markers_horizontal * num_markers_vertical)
+if remainder:
+    sheet_count += 1
 
-# Handling leftover markers
-remaining_markers = len(box_marker_paths) % (markers_per_row * markers_per_col)
-if remaining_markers:
-    markers_for_last_sheet = box_marker_paths[-remaining_markers:]
-    output_sheet_path = f"Markers/{marker_size_cm}cm/{marker_size_cm}cm sheets/sheet_{sheet_count+1}.png"
-    generate_A4_sheet_with_markers(markers_for_last_sheet, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, output_sheet_path)
+for i in range(sheet_count):
+    current_page = i + 1
+    sheet_path = os.path.join(sheet_folder, f"sheet_{current_page}.png")
+    start_idx = i * num_markers_horizontal * num_markers_vertical    
+    generate_A4_sheet_with_markers(box_marker_paths, start_idx, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, sheet_path, current_page, total_pages)    
 
 print("All tasks complete!")
