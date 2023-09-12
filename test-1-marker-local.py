5aa6b5729247bce8f3646d80351e28a15e3f160f
Karl <karl.spiteri777@gmail.com>
2023-09-12 14:59:16 +0200

    rec

--- test-1-marker-local.py
+++ test-1-marker-local.py
@@ -3,6 +3,7 @@ import time
 import asyncio
 import websockets
 import socket
+import os
 import cv2
 import numpy as np
 # import tensorflow as tf
@@ -12,51 +13,58 @@ import numpy as np
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_100)
 parameters = cv2.aruco.DetectorParameters_create()
 
+# Rec
+rec_directory = "Calibration\\Rec"
+frame_width, frame_height = 640, 480
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
 # Define the size of the markers (in meters)
 marker_size = 0.05
 
 red = (0, 0, 255)
-green = (0, 255, 0)
+green = (0, 127, 0)
 blue = (255, 0, 0)
 
 cap = cv2.VideoCapture(0)
 
-# Define the camera matrix and distortion coefficients
-# camera_matrix = np.array(
-#     [
-#         [6.73172250e02, 0.00000000e00, 3.21652381e02],
-#         [0.00000000e00, 6.73172250e02, 2.40854103e02],
-#         [0.00000000e00, 0.00000000e00, 1.00000000e00],
-#     ]
-# )
-# distortion_coefficients = np.array(
-#     [-2.87888863e-01, 9.67075352e-02, 1.65928771e-03, -5.19671229e-04, -1.30327183e-02]
-# )
-
-# camera_matrix = np.loadtxt("Calibration\\Pics\\Final\\camera_matrix_hp.txt")
-# distortion_coefficients = np.loadtxt("Calibration\\Pics\\Final\\distortion_coefficients_hp.txt")
+camera_matrix = np.loadtxt("Calibration\\Pics\\Final\\camera_matrix_hp.txt")
+distortion_coefficients = np.loadtxt("Calibration\\Pics\\Final\\distortion_coefficients_hp.txt")
 
 # camera_matrix = np.loadtxt("Calibration\\Pics\\Final\\camera_matrix_hp_OLD.txt")
 # distortion_coefficients = np.loadtxt("Calibration\\Pics\\Final\\distortion_coefficients_hp_OLD.txt")
 
-camera_matrix = np.loadtxt("Calibration\\Pics\\Final\\camera_matrix_hp_first.txt")
-distortion_coefficients = np.loadtxt("Calibration\\Pics\\Final\\distortion_coefficients_hp_first.txt")
+# camera_matrix = np.loadtxt("Calibration\\Pics\\Final\\camera_matrix_hp_first.txt")
+# distortion_coefficients = np.loadtxt("Calibration\\Pics\\Final\\distortion_coefficients_hp_first.txt")
 
 print("Main Camera Matrix:\n", camera_matrix)
 print("\nMain Distortion Coefficients:\n", distortion_coefficients)
 
 def receive_frames():
+    ############################## Create Rec Folders #########################################
+    
+    att_folders = [folder for folder in os.listdir(rec_directory) if folder.startswith("att")]
+    att_numbers = [int(folder.split("_")[1]) for folder in att_folders if len(folder.split("_")) == 2]
+
+    if not att_numbers:
+        next_att_number = 1
+    else:
+        next_att_number = max(att_numbers) + 1
+
+    new_att_folder = os.path.join(rec_directory, f"att_{next_att_number}")
+
+    try:
+        os.mkdir(new_att_folder)
+        print(f"Created folder: {new_att_folder}")
+    except OSError as e:
+        print(f"Failed to create folder: {new_att_folder}. Error: {e}")    
+    
     while True:
         ret, frame = cap.read()
-
+        
         # Check if the frame was read successfully
         if not ret:
             print("Error: Could not read frame.")
-            break
-
-        frame_width = 640
-        frame_height = 480
-        size = (frame_width, frame_height)
+            break        
 
         ############################## ArUco Detection #########################################
 
@@ -99,12 +107,12 @@ def receive_frames():
                     )   
 
                     # Translation matrix
-                    TVX = f"ID{ids[i]}: Translation Vector X: {tvecs[i][0][0]} meters"
-                    TVY = f"ID{ids[i]}: Translation Vector Y: {tvecs[i][0][1]} meters"
-                    TVZ = f"ID{ids[i]}: Translation Vector Z: {tvecs[i][0][2]} meters"
+                    # TVX = f"ID{ids[i]}: Translation Vector X: {tvecs[i][0][0]} meters"
+                    # TVY = f"ID{ids[i]}: Translation Vector Y: {tvecs[i][0][1]} meters"
+                    # TVZ = f"ID{ids[i]}: Translation Vector Z: {tvecs[i][0][2]} meters"
 
-                    xdef = 120
-                    ydef = 150
+                    xdef = 10 # 120
+                    ydef = 20 # 150
                     yoff = 25
                     size = 0.4
                     thick = 1
@@ -139,45 +147,58 @@ def receive_frames():
                         thick,
                     )
 
-                    cv2.putText(
-                        frame,
-                        TVX,
-                        (xdef, ydef + (yoff * 3) + 9),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        size,
-                        red,
-                        thick,
-                    )
+                    # cv2.putText(
+                    #     frame,
+                    #     TVX,
+                    #     (xdef, ydef + (yoff * 3) + 9),
+                    #     cv2.FONT_HERSHEY_SIMPLEX,
+                    #     size,
+                    #     red,
+                    #     thick,
+                    # )
 
-                    cv2.putText(
-                        frame,
-                        TVY,
-                        (xdef, ydef + (yoff * 4) + 9),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        size,
-                        green,
-                        thick,
-                    )
+                    # cv2.putText(
+                    #     frame,
+                    #     TVY,
+                    #     (xdef, ydef + (yoff * 4) + 9),
+                    #     cv2.FONT_HERSHEY_SIMPLEX,
+                    #     size,
+                    #     green,
+                    #     thick,
+                    # )
 
-                    cv2.putText(
-                        frame,
-                        TVZ,
-                        (xdef, ydef + (yoff * 5) + 9),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        size,
-                        blue,
-                        thick,
-                    )
+                    # cv2.putText(
+                    #     frame,
+                    #     TVZ,
+                    #     (xdef, ydef + (yoff * 5) + 9),
+                    #     cv2.FONT_HERSHEY_SIMPLEX,
+                    #     size,
+                    #     blue,
+                    #     thick,
+                    # )
 
                     # cv2.putText(image, f"{label}: {int(score*100)} %", (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
                 
         # Display output
         cv2.imshow("Results", frame)
-
+        
+        # Create a VideoWriter object to save the video
+        video_filename = f"my_video_{next_att_number}.avi"  # Change the filename and format as needed
+        video_path = os.path.join(new_att_folder, video_filename)        
+        video_out = cv2.VideoWriter(video_path, fourcc, 20.0, (frame_width, frame_height))
+        
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             cv2.destroyAllWindows()
+            video_out.release()  # Release the VideoWriter when done
+            cap.release()
             break            
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     receive_frames()
+
+
+
+
+
+
