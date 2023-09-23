43ef0ae3f3782ed3049d18edd52dc2e61c859a9d
Karl Spiteri <karl.spiteri.e21724@mcast.edu.mt>
2023-09-23 02:55:22 +0200

    marker_gen

--- markers-new-v2.py
+++ markers-new-v2.py
@@ -5,9 +5,125 @@ import math
 import numpy as np
 import cv2.aruco as aruco
 
-def generate_aruco_marker(marker_id, markerDic, marker_size_cm, resolution_ppcm, image_path):
+def get_valid_integer_input(prompt, min_value=None, max_value=None):
+    while True:
+        try:
+            user_input = int(input(prompt))
+            if (min_value is None or user_input >= min_value) and (max_value is None or user_input <= max_value):
+                return user_input
+            else:
+                print(f"Input must be between {min_value} and {max_value} (inclusive).")
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
+
+def get_valid_float_input(prompt, min_value=None, max_value=None):
+    while True:
+        try:
+            user_input = float(input(prompt))
+            if (min_value is None or user_input >= min_value) and (max_value is None or user_input <= max_value):
+                return user_input
+            else:
+                print(f"Input must be between {min_value} and {max_value} (inclusive).")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+
+def get_valid_bool_input(prompt):
+    while True:
+        user_input = input(prompt)
+        if user_input.lower() == "yes" or user_input.lower() == "1":
+            return True
+        elif user_input.lower() == "no" or user_input.lower() == "0":
+            return False
+        else:
+            print("Invalid input! Please enter 'Yes' or 'No' (or '1' or '0').")
+
+def select_paper_size(paper_size, paper_margin_cm=1):
+    dimensions = {
+        0: (84.1 - paper_margin_cm, 118.9 - paper_margin_cm),
+        1: (59.4 - paper_margin_cm, 84.1 - paper_margin_cm),
+        2: (42.0 - paper_margin_cm, 59.4 - paper_margin_cm),
+        3: (29.7 - paper_margin_cm, 42.0 - paper_margin_cm),
+        4: (21.0 - paper_margin_cm, 29.7 - paper_margin_cm)
+    }
+
+    return dimensions.get(paper_size, (None, None))
+
+def user_inputs():
+    while True:  
+        try:
+            paper_size = get_valid_integer_input("Enter the paper size (A0 (0) to A4 (4)): ", min_value = 0, max_value = 4)
+        except ValueError:
+            print("Invalid input! Please enter a valid paper size between 0 and 4.")
+            
+        try:
+            marker_dic = get_valid_integer_input("Enter marker dictionary (4-7): ", min_value = 4, max_value = 7)
+        except ValueError:
+            print("Invalid input! Please enter a valid marker size between 4 and 7.")     
+            
+        try:
+            marker_size_cm = get_valid_float_input("Enter the marker physical size in cm: ")
+        except ValueError:
+            print("Invalid input! Please enter a valid marker size in cm.")     
+        
+        #-------In progress-------        
+        # try:
+        #     num_inputs = get_valid_integer_input("Enter the number of marker physical sizes in cm to generate: ", min_value = 1)
+        # except ValueError:
+        #     print("Invalid input! Please enter a valid paper size between 0 and 4.")        
+        
+        # physical_sizes_list = []
+        # for i in range(num_inputs):
+        #     try:
+        #         user_input = get_valid_float_input(f"Enter the marker physical size in cm for input {i + 1}: ")
+        #         physical_sizes_list.append(user_input)
+        #     except ValueError:
+        #         print("Invalid input! Please enter a valid number.")
+        #         #make exit maybe
+        #-------In progress-------
+        
+        print("\nSelect an ID range type:")
+        print("1. Single Range (single value)")
+        print("2. Custom Range (start to end)")
+        print("3. Multiple Ranges (multiple start and end)")
+        
+        choice = input("Enter your choice (1/2/3): ")
+        
+        if choice == '1':
+            try:
+                value = get_valid_integer_input("\nEnter a single value: ")
+                return paper_size, marker_dic, marker_size_cm, choice, value + 1
+            except ValueError:
+                print("\nInvalid input! Please enter a valid integer.")
+        elif choice == '2':
+            try:
+                start = get_valid_integer_input("\nEnter the start of the range: ")
+                end = get_valid_integer_input("Enter the end of the range: ")
+                if start <= end:
+                    return paper_size, marker_dic, marker_size_cm, choice, list(range(start, end + 1))
+                else:
+                    print("\nInvalid range! Start should be less than or equal to end.")
+            except ValueError:
+                print("\nInvalid input! Please enter valid integers for start and end.")
+        elif choice == '3':
+            try:
+                num_ranges = get_valid_integer_input("\nEnter the number of ranges: ")
+                ranges = []
+                for i in range(num_ranges):
+                    start = get_valid_integer_input(f"\nEnter the start of range {i + 1}: ")
+                    end = get_valid_integer_input(f"Enter the end of range {i + 1}: ")
+                    if start <= end:
+                        ranges.append(list(range(start, end + 1)))
+                    else:
+                        print(f"\nInvalid range for range {i + 1}! Start should be less than or equal to end.")
+                return paper_size, marker_dic, marker_size_cm, choice, ranges
+            except ValueError:
+                print("\nInvalid input! Please enter valid integers for ranges.")
+        else:
+            print("\nInvalid choice! Please select 1, 2, or 3.")
+
+def generate_aruco_marker(marker_id, marker_dic, marker_size_cm, resolution_ppcm, image_path):
     marker_size_pixels_res = int(marker_size_cm * resolution_ppcm)
-    key = getattr(cv2.aruco, f'DICT_{markerDic}X{markerDic}_1000')    
+    key = getattr(cv2.aruco, f'DICT_{marker_dic}X{marker_dic}_1000')    
     aruco_dict = aruco.getPredefinedDictionary(key)
     marker_image = aruco.drawMarker(aruco_dict, marker_id, marker_size_pixels_res)
     cv2.imwrite(image_path, marker_image)
@@ -21,22 +137,24 @@ def generate_white_box(box_size_cm, resolution_ppcm, output_path, border_thickne
     box_image[:, -border_thickness:] = 0
     cv2.imwrite(output_path, box_image)
 
-def generate_A4_sheet_with_markers(printBool, box_marker_paths, start_idx, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, output_path, current_page, total_pages, margin_cm, markerSizeStart, fontScale, thickness):
-    A4_width_pixels = int(A4_width_cm * resolution_ppcm)
-    A4_height_pixels = int(A4_height_cm * resolution_ppcm)
+def generate_sheet_with_markers(printBool, box_marker_paths, start_idx, paper_width_cm, paper_height_cm, box_size_cm, resolution_ppcm, output_path, current_page, total_pages, margin_cm, marker_dic):
+    
+    fontScale, thickness = 0.35, 1    
+    width_pixels = int(paper_width_cm * resolution_ppcm)
+    height_pixels = int(paper_height_cm * resolution_ppcm)
     
     if printBool:
         print("\n")
-        print(f"A4_width_pixels: {A4_width_pixels}")
-        print(f"A4_height_pixels: {A4_height_pixels}")
+        print(f"width_pixels: {width_pixels}")
+        print(f"height_pixels: {height_pixels}")
 
-    A4_image = np.ones((A4_height_pixels, A4_width_pixels), dtype=np.uint8) * 255
+    sheet_image = np.ones((height_pixels, width_pixels), dtype=np.uint8) * 255
 
     box_marker_pixel_size = int(box_size_cm * resolution_ppcm)
     if printBool: print(f"box_marker_pixel_size: {box_marker_pixel_size}")
     
-    num_markers_horizontal = int(A4_width_cm // (box_size_cm + margin_cm))
-    num_markers_vertical = int(A4_height_cm // (box_size_cm + margin_cm))
+    num_markers_horizontal = int(paper_width_cm // (box_size_cm + margin_cm))
+    num_markers_vertical = int(paper_height_cm // (box_size_cm + margin_cm))
     
     if printBool: 
         print(f"num_markers_horizontal: {num_markers_horizontal}")
@@ -49,8 +167,8 @@ def generate_A4_sheet_with_markers(printBool, box_marker_paths, start_idx, A4_wi
         print(f"total_width_required: {total_width_required}")
         print(f"total_height_required: {total_height_required}")
     
-    start_x = (A4_width_cm - total_width_required) / 2 * resolution_ppcm
-    start_y = (A4_height_cm - total_height_required) / 2 * resolution_ppcm
+    start_x = (paper_width_cm - total_width_required) / 2 * resolution_ppcm
+    start_y = (paper_height_cm - total_height_required) / 2 * resolution_ppcm
     
     if printBool: 
         print(f"start_x: {start_x}")
@@ -66,7 +184,7 @@ def generate_A4_sheet_with_markers(printBool, box_marker_paths, start_idx, A4_wi
                 break
             box_marker_path = box_marker_paths[current_idx]
             box_marker_img = cv2.imread(box_marker_path, cv2.IMREAD_GRAYSCALE)
-            A4_image[int(y_coord):int(y_coord+box_marker_pixel_size), int(x_coord):int(x_coord+box_marker_pixel_size)] = box_marker_img
+            sheet_image[int(y_coord):int(y_coord+box_marker_pixel_size), int(x_coord):int(x_coord+box_marker_pixel_size)] = box_marker_img
             x_coord += (box_size_cm + margin_cm) * resolution_ppcm
             current_idx += 1
         y_coord += (box_size_cm + margin_cm) * resolution_ppcm
@@ -82,99 +200,108 @@ def generate_A4_sheet_with_markers(printBool, box_marker_paths, start_idx, A4_wi
     if printBool: 
         print(f"start_id: {start_id}")
         print(f"end_id: {end_id}")
-
-    marker_name = f"{markerSizeStart}x{markerSizeStart} Markers ID {start_id} - {end_id}"  
-    cv2.putText(A4_image, marker_name, (30, A4_height_pixels - 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 0, 0), thickness, cv2.LINE_AA)
     
-    page_number = f"Page {current_page}/{total_pages}"
-    cv2.putText(A4_image, page_number, (A4_width_pixels - 200, A4_height_pixels - 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0,0,0), thickness, cv2.LINE_AA)
+    marker_dic_name = f"{marker_dic}x{marker_dic}"  
+    cv2.putText(sheet_image, marker_dic_name, (width_pixels - 40, height_pixels - 110), cv2.FONT_HERSHEY_SIMPLEX, fontScale * 1.5, (0, 0, 0), thickness, cv2.LINE_AA)
     
-    cv2.imwrite(output_path, A4_image)
+    size_cm = f"{box_size_cm-0.4}"
+    cv2.putText(sheet_image, size_cm, (width_pixels - 40, height_pixels - 85), cv2.FONT_HERSHEY_SIMPLEX, fontScale * 1.5, (0,0,0), thickness, cv2.LINE_AA)
+    
+    marker_id_range = f"{start_id}-{end_id}"
+    cv2.putText(sheet_image, marker_id_range, (width_pixels - 40, height_pixels - 60), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 0, 0), thickness, cv2.LINE_AA)
+    
+    page_number = f"{current_page}/{total_pages}"
+    cv2.putText(sheet_image, page_number, (width_pixels - 40, height_pixels - 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale * 1.5, (0,0,0), thickness, cv2.LINE_AA)
+    
+    cv2.imwrite(output_path, sheet_image)
 
-    if printBool: print(f"Saved A4 sheet with markers to: {output_path}")
+    if printBool: print(f"Saved sheet with markers to: {output_path}")
 
-try:
-    markerDic = int(input("Enter marker dictionary (4-7): "))
-    if markerDic < 4 or markerDic > 7:
-        raise ValueError
-except ValueError:
-    print("Invalid input! Please enter a valid marker size between 4 and 7.")
-    exit()
+def all_generate(marker_dic, marker_size_cm, paper_width_cm, paper_height_cm, resolution_ppcm, margin_cm, marker_num, debug = 0):  # marker_num still in progress
+    
+    box_size_cm = marker_size_cm + 0.4    
+    num_markers_horizontal = int(paper_width_cm // (box_size_cm + margin_cm))
+    num_markers_vertical = int(paper_height_cm // (box_size_cm + margin_cm))
 
-try:
-    marker_size_cm = float(input("Enter the marker physical size in cm: "))
-except ValueError:
-    print("Invalid input! Please enter a valid number.")
-    exit()
+    if num_markers_horizontal < 1 or num_markers_vertical < 1:
+        print("The markers are too large for the selected size.")
+        exit()
 
-try:
-    debug = float(input("Debug Print Yes or No (1 or 0): "))
-except ValueError:
-    print("Invalid input! Please enter a valid number.")
-    exit()
+    base_path = os.path.join("Markers", f"{marker_size_cm}cm")
+    output_path = os.path.join(base_path, "white_box.png")    
+    # marker_num = 16 + 1
 
-fontScale, thickness = 1, 2
-box_size_cm = marker_size_cm + 0.4
-margin_cm = 1 * 2
-A4_width_cm, A4_height_cm = 21 - 2, 29.7 - 2
+    os.makedirs(os.path.join(base_path, "Generated_Markers"), exist_ok=True)
+    os.makedirs(os.path.join(base_path, "Generated_Box_Markers"), exist_ok=True)
+    generate_white_box(box_size_cm, resolution_ppcm, output_path)
+    
+    for i in range(marker_num):
+    #for i in range(len(marker_num)):
+        marker_path = os.path.join(base_path, "Generated_Markers", f"{marker_dic}x{marker_dic}_marker_id{i}_{marker_size_cm}cm.png")
+        generate_aruco_marker(i, marker_dic, marker_size_cm, resolution_ppcm, marker_path)
 
-num_markers_horizontal = int(A4_width_cm // (marker_size_cm + margin_cm))
-num_markers_vertical = int(A4_height_cm // (marker_size_cm + margin_cm))
+    for i in range(marker_num):
+        background = cv2.imread(output_path)
+        marker_path = os.path.join(base_path, "Generated_Markers", f"{marker_dic}x{marker_dic}_marker_id{i}_{marker_size_cm}cm.png")
+        overlay = cv2.imread(marker_path)
+        y, x = tuple((background.shape[d] - overlay.shape[d]) // 2 for d in range(2))
+        h, w = overlay.shape[:2]
+        background[y:y+h, x:x+w] = overlay
+        box_marker_path = os.path.join(base_path, "Generated_Box_Markers", f"{marker_dic}x{marker_dic}_marker_id{i}_{marker_size_cm}cm.png")
+        cv2.imwrite(box_marker_path, background)
 
-if num_markers_horizontal < 1 or num_markers_vertical < 1:
-    print("The markers are too large for the A4 size.")
-    exit()
+    sheet_folder = os.path.join(base_path, f"{marker_size_cm}cm sheets")
+    os.makedirs(sheet_folder, exist_ok=True)
 
+    box_marker_paths = [os.path.join(base_path, "Generated_Box_Markers", f"{marker_dic}x{marker_dic}_marker_id{i}_{marker_size_cm}cm.png") 
+                        for i in range(marker_num)]
+
+    # Calculate the number of markers on a single sheet
+    markers_per_sheet = num_markers_horizontal * num_markers_vertical
+
+    # Calculate the total number of sheets required
+    sheet_count = math.ceil(len(box_marker_paths) / markers_per_sheet)
+
+    if debug:
+        print("\n")
+        print(f"len(box_marker_paths): {len(box_marker_paths)}\n")
+        print(f"num_markers_horizontal: {num_markers_horizontal}\n")
+        print(f"num_markers_vertical: {num_markers_vertical}\n")
+        print(f"markers_per_sheet: {markers_per_sheet}\n")
+        print(f"sheet_count: {sheet_count}\n")
+
+    for i in range(sheet_count):
+        current_page = i + 1
+        sheet_path = os.path.join(sheet_folder, f"{marker_size_cm}cm_sheet_{current_page}.png")
+        start_idx = i * markers_per_sheet
+        generate_sheet_with_markers(debug, box_marker_paths, start_idx, paper_width_cm, paper_height_cm, box_size_cm, resolution_ppcm, sheet_path, current_page, sheet_count, margin_cm, marker_dic)
+        
+    # Add delete for extra files and folders
+
+paper_size, marker_dic, marker_size_cm, selected_choice, selected_range = user_inputs()
+margin_cm = 0.75
+paper_margin_cm = 1
 resolution_ppcm = 37.8
-base_path = os.path.join("Markers", f"{marker_size_cm}cm")
-output_path = os.path.join(base_path, "white_box.png")
-markerSizeStart, markerSizeEnd = 6, 6
-num_markers = 16 + 1
+selected_width, selected_height = select_paper_size(paper_size, paper_margin_cm)
 
-os.makedirs(os.path.join(base_path, "Generated_Markers"), exist_ok=True)
-os.makedirs(os.path.join(base_path, "Generated_Box_Markers"), exist_ok=True)
-generate_white_box(box_size_cm, resolution_ppcm, output_path)
+#---------------------------------------------
+# try:
+#     debug = get_valid_bool_input("Debug Print Yes or No (Yes = 1 or No = 0): ")
+# except ValueError:
+#     print("Invalid input! Please enter 'Yes' or 'No' (or '1' or '0').")
+#---------------------------------------------
 
-#for i in range(markerSizeStart, markerSizeEnd+1):
-for i in range(num_markers):
-    marker_path = os.path.join(base_path, "Generated_Markers", f"{markerDic}x{markerDic}_marker_id{i}_{marker_size_cm}cm.png")
-    generate_aruco_marker(i, markerDic, marker_size_cm, resolution_ppcm, marker_path)
+#if debug: print(paper_size, marker_dic, marker_size_cm, selected_choice, selected_range)
 
-#for i in range(markerSizeStart, markerSizeEnd+1):
-for i in range(num_markers):
-    background = cv2.imread(output_path)
-    marker_path = os.path.join(base_path, "Generated_Markers", f"{markerDic}x{markerDic}_marker_id{i}_{marker_size_cm}cm.png")
-    overlay = cv2.imread(marker_path)
-    y, x = tuple((background.shape[d] - overlay.shape[d]) // 2 for d in range(2))
-    h, w = overlay.shape[:2]
-    background[y:y+h, x:x+w] = overlay
-    box_marker_path = os.path.join(base_path, "Generated_Box_Markers", f"{markerDic}x{markerDic}_marker_id{i}_{marker_size_cm}cm.png")
-    cv2.imwrite(box_marker_path, background)
+print(selected_range)
 
-sheet_folder = os.path.join(base_path, f"{marker_size_cm}cm sheets")
-os.makedirs(sheet_folder, exist_ok=True)
-
-box_marker_paths = [os.path.join(base_path, "Generated_Box_Markers", f"{markerDic}x{markerDic}_marker_id{i}_{marker_size_cm}cm.png") 
-                    for i in range(num_markers)]
-
-# Calculate the number of markers on a single sheet
-markers_per_sheet = num_markers_horizontal * num_markers_vertical
-
-# Calculate the total number of sheets required
-sheet_count = math.ceil(len(box_marker_paths) / markers_per_sheet)
-
-if debug:
-    print("\n")
-    print(f"len(box_marker_paths): {len(box_marker_paths)}\n")
-    print(f"num_markers_horizontal: {num_markers_horizontal}\n")
-    print(f"num_markers_vertical: {num_markers_vertical}\n")
-    print(f"markers_per_sheet: {markers_per_sheet}\n")
-    print(f"sheet_count: {sheet_count}\n")
-
-for i in range(sheet_count):
-    current_page = i + 1
-    sheet_path = os.path.join(sheet_folder, f"{marker_size_cm}cm_sheet_{current_page}.png")
-    start_idx = i * markers_per_sheet
-    generate_A4_sheet_with_markers(debug, box_marker_paths, start_idx, A4_width_cm, A4_height_cm, box_size_cm, resolution_ppcm, sheet_path, current_page, sheet_count, margin_cm, markerSizeStart, fontScale, thickness)
-
-print("\nAll tasks complete!")
+if selected_choice == '1':
+    all_generate(marker_dic, marker_size_cm, selected_width, selected_height, resolution_ppcm, margin_cm, selected_range)   
+    
+elif selected_choice == '2':
+    print("This is case 2")
+    
+elif selected_choice == '3':
+    print("This is case 3")
+   
+print("\nAll tasks complete!\n")
